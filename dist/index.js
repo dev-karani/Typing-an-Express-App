76010c9a963b1fff160df577b6c9b8a40c58cde9
dev-karani <edwin.k.kithinji@gmail.com>
2026-01-07 18:08:44 +0300

    feat:Typed catc all route

--- dist/index.js
+++ dist/index.js
@@ -32,10 +32,15 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importStar(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const Port = 8000;
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     express_1.response.json({});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import express, { response } from "express";
+import express, {Request, Response } from "express";
 import type {Express} from 'express'
 import { pets } from "./data/pets";
+import cors from 'cors'
+import type { Pet } from "./data/pets";
 
 const app:Express = express()
 const Port:number = 8000
 
+app.use(cors())
 
-app.get('/', (req,res)=>{
-    response.json({})
+app.get('/', (req:Request, res: Response<Pet[]>)=>{
+    res.json(pets)
 })
-
+app.use((req:Request, res:Response):void=>{
+    res.json({message:"endpoint not found"}).status(404)
+})
 app.listen(Port, ():void=> console.log(`Listening on port ${Port}`));
